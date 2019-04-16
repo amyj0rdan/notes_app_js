b99b77ae73ca404e49f23339052384683eb33993
Amy Jordan <amy_jordan@live.co.uk>
2019-04-16 15:27:00 +0100

    Spike to create NoteController - will now TDD

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,16 @@
-let appElem = document.getElementById("app");
-appElem.innerHTML = "howdy";
-console.log(appElem);
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+  }
+
+  NoteController.prototype.addNote = function (text) {
+    return this.noteList.addNote(text);
+  };
+
+  NoteController.prototype.insertHTML = function () {
+    let noteListView = new NoteListView(this.noteList);
+    document.getElementById("app").innerHTML = noteListView.returnHTML();
+  };
+
+  exports.NoteController = NoteController;
+})(this);
